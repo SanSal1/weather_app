989dbb7ab6f93e12de7fd29486329ef899e94ea3
Sanna Salonen <sanna.m.salonen@tuni.fi>
2022-02-14 11:32:56 +0200

    added comments

--- frontend/src/components/Cities.jsx
+++ frontend/src/components/Cities.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import City from './City';
 
 
+/**
+ * Component that lists and displays cities based on given ids.
+ * 
+ * @param {array} visibleCities array of ids for cities that should be visible
+ * @param {function} handleError parent is called in case an error happens during a fetch
+ */
 const Cities = ({ visibleCities, handleError }) => {
   return (
     <ul id='cities-container'> {visibleCities.map((city) => (
--- frontend/src/data/errorMessages.js
+++ frontend/src/data/errorMessages.js
@@ -1,6 +1,6 @@
 const errorMessages = {
   0: 'Unknown error. Please try again later.',
-  409: 'Forbidden. Please request access to the proxy.',
+  409: 'Request forbidden.',
   429: 'Too many requests. Please wait a minute before refreshing the page.'
 }
 
